ab8f7877365bfffd1c311c424d5405b0a526b64b
Md Musfiqur Rahman <mosfiqurrahman@students.diu.ac>
2025-10-31 19:00:57 +0600

    Add files via upload

---
+++ Operation.js
@@ -0,0 +1,28 @@
+console.log(2**3); // Exponentiation
+
+console.log(10 % 3); // Modulus
+
+console.log(Math.sqrt(16)); // Square root
+
+console.log(Math.abs(-7)); // Absolute value
+
+console.log(Math.round(4.4)); // Rounding
+console.log(Math.round(4.6)); // Rounding
+
+console.log(Math.ceil(4.2)); // Ceiling
+console.log(Math.floor(4.8)); // Floor
+
+console.log(Math.min(3, 1, 4, 2)); // Minimum
+console.log(Math.max(3, 1, 4, 2)); // Maximum
+
+console.log(Math.random()); // Random number between 0 and 1
+console.log(Math.floor(Math.random() * 100) + 1); // Random number between 1 and 100
+
+
+
+
+
+
+
+
+
