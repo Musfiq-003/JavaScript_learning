031e9850141f270235c08a8250edb21706d337cd
Md Musfiqur Rahman <mosfiqurrahman@students.diu.ac>
2025-10-31 19:46:14 +0600

    Add various mathematical and string operations

--- Operation.js
+++ Operation.js
@@ -18,9 +18,52 @@ console.log(Math.max(3, 1, 4, 2)); // Maximum
 console.log(Math.random()); // Random number between 0 and 1
 console.log(Math.floor(Math.random() * 100) + 1); // Random number between 1 and 100
 
+let st1r = "Hello";
+let st2r = "World";
+console.log(st1r + " " + st2r); // String concatenation
+
+let num1 = 10;
+let num2 = 20;
+console.log(num1 + num2); // Numeric addition
+
+console.log("1" + 2); // String and number concatenation
+
+console.log("1" + 2 + 3); // Left to right evaluation
+
+console.log(1 + 2 + "3"); // Left to right evaluation
+
+console.log(+true); // Unary plus
+console.log(+false); // Unary plus
+console.log(+"123"); // Unary plus
+console.log(+""); // Unary 
+console.log(+null); // Unary plus
+console.log(+undefined); // Unary plus
 
 
+let x = 5;
+++x; // Increment
+console.log(x);
 
+let y = 5;
+y++; // Increment
+console.log(y);
+
+let a = 5;
+--a; // Decrement
+console.log(a);
+
+let b = 5;
+b--; // Decrement
+console.log(b);
+
+
+Postfix increment
+let x = 3;
+const y = x++; // x is 4; y is 3
+
+Prefix increment
+let x = 3;
+const y = ++x; // x is 4; y is 4
 
 
 
