c1375fc42edaf1b035f7663d2234f169c661ed22
Md Musfiqur Rahman <mosfiqurrahman@students.diu.ac>
2025-10-31 19:00:33 +0600

    Add files via upload

---
+++ Convertion.js
@@ -0,0 +1,17 @@
+let isAdult = true;
+let isStudent = Number(isAdult)
+console.log(isStudent);
+
+let score = "100";
+let convertedScore = Number(score);
+console.log(convertedScore);
+
+let isPassed = Boolean(convertedScore);
+console.log(isPassed);
+
+let stringValue = String(convertedScore);
+console.log(stringValue);
+
+let value = 15
+let negativeValue = -value
+console.log(negativeValue);
