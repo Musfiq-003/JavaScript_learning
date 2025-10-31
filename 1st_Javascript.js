c4d520aa65699d1ffc7dce095f2b43b2a3b7e3b6
Md Musfiqur Rahman <mosfiqurrahman@students.diu.ac>
2025-10-31 18:59:44 +0600

    Add files via upload

---
+++ 1st_Javascript.js
@@ -0,0 +1,25 @@
+
+let Name ="John Doe"
+let Age=30
+let Country="USA"
+let Email="example@example.com"
+let Hobby
+Age=31
+Email="example@exampl.com"
+
+console.log("Try programiz pro");
+console.log("Name: " + Name);
+console.log("Age: " + Age);
+console.log("Country: " + Country);
+console.log("Email: " + Email);
+
+console.table({Name, Age, Country, Email, Hobby});
+
+// alert("Welcome to Portfolio Page");
+
+console.log(typeof Email);
+console.log(typeof Age);
+console.log(typeof null);
+
+
+
