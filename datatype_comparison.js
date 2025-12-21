6c67bfcf8ea99927ea6d3d63d34514e3c7675b04
Md Musfiqur Rahman <mosfiqurrahman@students.diu.ac>
2025-12-21 17:53:21 +0600

    Add files via upload

---
+++ datatype_comparison.js
@@ -0,0 +1,29 @@
+
+console.log("Datatype Comparison in JavaScript");
+
+console.log('"5" == 5 :', "5" == 5);
+console.log('"5" === 5 :', "5" === 5);
+
+//null comparisons
+console.log('null == 0 :', null == 0);
+console.log('null === 0 :', null === 0);
+console.log('null <= 0 :', null <= 0);
+console.log('null >= 0 :', null >= 0);
+console.log('null < 0 :', null < 0);
+console.log('null > 0 :', null > 0);
+
+//undefined comparisons
+console.log('undefined == 0 :', undefined == 0);
+console.log('undefined === 0 :', undefined === 0);
+console.log('undefined <= 0 :', undefined <= 0);
+console.log('undefined >= 0 :', undefined >= 0);
+console.log('undefined < 0 :', undefined < 0);
+console.log('undefined > 0 :', undefined > 0);
+
+//null and undefined comparisons
+console.log('null == undefined :', null == undefined);
+console.log('null === undefined :', null === undefined);
+console.log('null <= undefined :', null <= undefined);
+console.log('null >= undefined :', null >= undefined);
+console.log('null < undefined :', null < undefined);
+console.log('null > undefined :', null > undefined);
